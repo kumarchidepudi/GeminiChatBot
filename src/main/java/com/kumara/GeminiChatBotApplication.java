4c32b2417b80c9a7cf26a1c5334e7e5327d8d687
kumarchidepudi <venkat.chidepudi@gmail.com>
2025-10-08 16:04:30 +0530

    refactor: clean up formatting in main method

--- src/main/java/com/kumara/GeminiChatBotApplication.java
+++ src/main/java/com/kumara/GeminiChatBotApplication.java
@@ -1,9 +1,5 @@
 package com.kumara;
 
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.google.genai.GoogleGenAiChatModel;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,11 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GeminiChatBotApplication {
 
 
+    public static void main(String[] args) {
+        SpringApplication.run(GeminiChatBotApplication.class, args);
 
-	public static void main(String[] args) {
-		SpringApplication.run(GeminiChatBotApplication.class, args);
-
-	}
+    }
 
 
 }
